3a579d39740fb5f8cf13ba74381e337fe75a6398
KafuiAdaku <dennisadaku@ymail.com>
2022-12-08 06:29:30 -0800

    shell script that prints the size of various data types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	long int l;
+	long long int n;
+	char c;
+	float f;
+	printf("Size of a char: %u byte(s)\n", unsigned(sizeof(f)));
+	printf("Size of an int: %u byte(s)\n", unsigned(sizeof(i)));
+	printf("Size of a long int: %u byte(s)\n", unsigned(sizeof(l)));
+	printf("Size of a long long int: %u byte(s)\n", unsigned(sizeof(n)));
+	printf("Size of a float: %u byte(s)\n", unsigned(sizeof(f)));
+	return (0);
+}
