3f62ec2757f40ff2086daa4c23c57fcc959a688a
KafuiAdaku <dennisadaku@ymail.com>
2022-12-09 08:15:00 -0800

    1-last_digit.c redo 3

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -26,7 +26,9 @@ int main(void)
 		printf("Last digit of %d is %d and is 0\n", n, last_digit);
 	}
 	else
+	{
 		printf("Last digit of %d is %d and is less than 6", n, last_digit);
        		printf(" and not 0\n");
+	}
 	return (0);
 }
