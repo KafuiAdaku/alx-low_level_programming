48c1a45bc55d669b75e6f56b1991c65ac130922e
KafuiAdaku <dennisadaku@ymail.com>
2022-12-09 11:33:27 -0800

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Succes)
+ */
+int main(void)
+{
+	char a;
+
+	for (a = '0'; a < 10; a++)
+	{
+		putchar(a);
+	}
+	for (a = 'a'; a <= 'f'; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
