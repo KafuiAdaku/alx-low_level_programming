d13c7e6631115afad97a1a48eda201f2168b140c
KafuiAdaku <dennisadaku@ymail.com>
2022-12-11 05:55:51 -0800

    Software is eating the World

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Printing all possible combibnationof two-digit numbers.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a, b, c, d;
+
+	for (a = 48; a < 58; a++)
+	{
+		for (b = 48; b < 58; b++)
+		{
+			for (c = 48; c < 58; c++)
+			{
+				for (d = 48; d < 58; d++)
+				{
+					if (((c + d) > (a + b) && (c >= a)) || (a < c))
+					{
+						putchar(a + '0');
+						putchar(b + '0');
+						putchar(' ');
+						putchar(c + '0');
+						putchar(d + '0');
+
+						if (((a + b + c + d) == 227) && (c == 57))
+						{
+							break;
+						}
+						else
+						{
+						putchar(',');
+						putchar(' ');
+						}
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
