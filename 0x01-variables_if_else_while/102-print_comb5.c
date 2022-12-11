b83e01e68a1dbdd6747957b80a14efa334226f53
KafuiAdaku <dennisadaku@ymail.com>
2022-12-11 07:27:54 -0800

    Software is eating the World redo 4

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,13 +9,13 @@ int main(void)
 {
 int a, b, c, d;
 
-for (a = 48; a < 58; a++)
+for (a = 48; a <= 57; a++)
 {
-	for (b = 48; b < 58; b++)
+	for (b = 48; b <= 57; b++)
 	{
-		for (c = 48; c < 58; c++)
+		for (c = 48; c <= 57; c++)
 		{
-			for (d = 48; d < 58; d++)
+			for (d = 48; d <= 57; d++)
 			{
 				if (((c + d) > (a + b) && (c >= a)) || (a < c))
 				{
@@ -24,6 +24,7 @@ for (a = 48; a < 58; a++)
 					putchar(' ');
 					putchar(c);
 					putchar(d);
+
 					if (((a + b + c + d) == 227) && (c == 57))
 					{
 						break;
