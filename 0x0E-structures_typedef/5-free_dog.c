8b4c3a3f42c8b16f6b7f30e9b9e7c464aacca3d6
KafuiAdaku <dennisadaku@ymail.com>
2023-01-16 11:03:32 -0800

    added solution to task 5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * free_dog - frees the dog.
+ * @d: Pointer to dog_t.
+ * Return: Nothing.
+ */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
