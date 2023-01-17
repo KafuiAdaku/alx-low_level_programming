c36c99ad9ea7fae35c3ee279fc74a9b5c37d8148
KafuiAdaku <dennisadaku@ymail.com>
2023-01-16 21:38:10 -0800

    4. _realloc redo

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -10,22 +10,26 @@
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	unsigned int i;
-	char *temp_p, dup_ptr;
+	char *temp_p;
+	char *dup_ptr;
 
 	dup_ptr = ptr;
 
 	if (new_size == old_size)
 		return (ptr);
-	if (ptr != NULL && new_size == 0))
+	if (ptr != NULL && new_size == 0)
 	{
 		free(ptr);
 		return (NULL);
 	}
-	if (ptr = NULL)
+	if (ptr == NULL)
 	{
 		return (malloc(new_size));
 	}
-
+	
+	temp_p = malloc(new_size);
+	if (temp_p == NULL)
+		return (NULL);
 	if (new_size < old_size)
 	{
 		for (i = 0; i < new_size; i++)
