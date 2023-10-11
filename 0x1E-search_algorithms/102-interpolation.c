7bb1154d7974fa275594857d6e2a72bb1e68f0ca
KafuiAdaku <dennisadaku@ymail.com>
2023-10-11 03:43:37 -0700

    task 9: refactored print statement

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -35,7 +35,7 @@ int interpolation_search(int *array, size_t size, int value)
 		pos = low + (((double)(high - low) / (array[high] - array[low])) *
 				(value - array[low]));
 
-		printf("Value checked array[%lu} = [%d]\n", pos, array[pos]);
+		printf("Value checked array[%lu] = [%d]\n", pos, array[pos]);
 		if (value < array[pos])
 			high = pos - 1;
 		else if (value > array[pos])
